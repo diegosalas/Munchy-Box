9db87ba2c99f14cbd28d4d5d7afc3923eade4275
Diego Perez Salas <diegosalas@gmail.com>
2020-02-10 17:35:49 +0100

    changes

--- app/src/main/java/com/cabify/cabistore/ui/tag/TagFragment.kt
+++ app/src/main/java/com/cabify/cabistore/ui/tag/TagFragment.kt
@@ -1,9 +1,12 @@
 package com.cabify.cabistore.ui.tag
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -15,13 +18,18 @@ class TagFragment : Fragment() {
   private lateinit var binding: FragmentTagBinding
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    return super.onCreateView(inflater, container, savedInstanceState)
-
     binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tag, container, false)
-
-    binding.lifecycleOwner = this
+//    val application = requireNotNull(this.activity).application
+//
+//    requireActivity().onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+//      override fun handleOnBackPressed() {
+//        return
+//      }
+//    })
+//    binding.lifecycleOwner = this
 
     binding.btEnterTag.setOnClickListener {
+      (activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(view!!.windowToken, 0)
 
       findNavController().navigate(R.id.global_action)
     }
